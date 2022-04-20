b1259d6d97b254d9ff5966c8564d05dea4e9f88d
Keenan-Kimbrough <keenankimbrough@gmail.com>
2022-04-20 16:15:52 -0400

    updated input field

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
-import React, { Component } from 'react';
+import React from 'react';
 
 class App extends React.Component {
 
   constructor () {
 super()
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
   };
   console.log('constructor')
   }
@@ -25,24 +26,26 @@ console.log('componentDidMount')
   }
   render () {
 console.log('render')
+
+const filteredMonsters = this.state.monsters.filter((monster)=> {
+  return monster.name.toLocaleLowerCase().includes(this.state.searchField)
+
+})
     return (
 
       <div> 
   <input className = 'searchbox' type='search' placeholder= 'search monsters'
   onChange = { (event)=> {
-    const searchString = event.target.value.toLocaleLowerCase();
-    const filteredMonsters = this.state.monsters.filter((monster)=> {
-      return monster.name.toLocaleLowerCase().includes(searchString)
-
-    })
+    const searchField = event.target.value.toLocaleLowerCase();
+    
     this.setState ( () => {
-      return { monsters: filteredMonsters}
+      return { searchField}
     })
 
   }}/>
 
 
- {this.state.monsters.map((monster) => {
+ {filteredMonsters.map((monster) => {
    return <h1 key={Math.random()}> {monster.name}</h1>
  })}
 
