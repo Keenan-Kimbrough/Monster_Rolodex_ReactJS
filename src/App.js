c8e49bd2f6692179b1d6a6fecb35c1174de27677
Keenan-Kimbrough <keenankimbrough@gmail.com>
2022-04-26 14:05:00 -0400

    changed app class componenent to functional component

--- src/App.js
+++ src/App.js
@@ -1,61 +1,76 @@
 
 import './App.css';
+import {useEffect, useState} from 'react'
 import React from 'react';
 import CardList from './components/card-list/cardlist.component';
 import SearchBox from './components/search-box/search-box.component';
 
-class App extends React.Component {
+const App = () => {
 
-  constructor () {
-super()
-    this.state = {
-      monsters: [],
-      searchField: ''
-  };
+  //array destructing , gives us back two values
+  const [searchField, setSearchField] = useState('')
+  const [monsters, setMonsters] = useState([])
+  const [filteredMonsters,setFilteredMonsters] =useState(monsters)
+  const[stringField, setStringField] = useState('')
+
+
+  // [value,setValue]
+  //use state is indivual objects
+  console.log(searchField)
+// takes 2 arguments, call back function and array of depencies
+// empty array means no depencies, just when it mounts
+
+
+  useEffect( () => {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then( (users)=> setMonsters(users) )
+
+  },[])
+
+  useEffect (() => {
+    const newFilteredMonsters = monsters.filter((monster)=> {
+      return monster.name.toLocaleLowerCase().includes(searchField)
+    
+    });
+    setFilteredMonsters(newFilteredMonsters);
+    console.log('effect is firing')
+  }, [monsters, searchField])
+
+ const onStringChange = (event) => {
+   setStringField(event.target.value);
+ }
   
-  }
 
-onSearchChange = (event)=> { 
-  const searchField = event.target.value.toLocaleLowerCase();
+const onSearchChange = (event)=> { 
+  const searchFieldString = event.target.value.toLocaleLowerCase();
+  setSearchField(searchFieldString)
+ 
+  }
   
-  this.setState ( () => {
-    return { searchField}
-  })
-}
 
-
-// API call
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then( (users)=> this.setState ( () => {
-      return {monsters: users}
-    }))
- 
-
-  }
-  render () {
-
-const {monsters, searchField} = this.state;
-
-
-
-const filteredMonsters = monsters.filter((monster)=> {
-  return monster.name.toLocaleLowerCase().includes(searchField)
-
-})
-    return (
-
-      <div className='App'> 
-      <h1 className='app-title'>Monsters Rolodex</h1>
-  <SearchBox onChangeHandler={this.onSearchChange} placeholder='placeholder' className='className'/>
- 
+return (
 
  
-  <CardList monsters= {filteredMonsters} anything={['a z']}/>
-      </div>
-    )
-  }
+
+<div className='App'> 
+<h1 className='app-title'>Monsters Rolodex</h1>
+<SearchBox 
+onChangeHandler={onSearchChange} 
+placeholder='placeholder' 
+className='className'/>
+
+<SearchBox 
+onChangeHandler={onStringChange} 
+placeholder='placeholder' 
+className='className'/>
+
+<CardList monsters= {filteredMonsters} anything={['a z']}/>
+
+
+</div>
+
+)  
 }
 
 export default App;
